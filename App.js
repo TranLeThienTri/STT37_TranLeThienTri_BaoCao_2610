d9be58db7c2fce17ffb603a070397ebebf7cb543
Tran Le Thien Tri <thientri.trank17@gmail.com>
2024-10-27 16:01:49 +0700

    update

--- App.js
+++ App.js
@@ -1,19 +1,13 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, StyleSheet } from "react-native";
 
 // You can import supported modules from npm
-import { Card } from 'react-native-paper';
+import { Card } from "react-native-paper";
 
 // or any files within the Snack
-import Screen from './components/screen.js';
+import Screen from "./components/screen.js";
 
 export default function App() {
-  return (
-    <SafeAreaView>
-      <Screen/>
-      </SafeAreaView>
-  );
+    return <Screen />;
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
--- components/screen.js
+++ components/screen.js
@@ -1,144 +1,358 @@
-import { Text, View, StyleSheet, Image, TextInput,FlatList } from 'react-native';
-import React, {useState, useEffect} from 'react';
-import axios from 'axios'
-import Footer from './footer'
-export default function Screen() {
+import React, { useEffect, useState } from "react";
+import {
+    View,
+    Text,
+    FlatList,
+    Image,
+    StyleSheet,
+    ScrollView,
+    TouchableOpacity,
+    Dimensions,
+    SafeAreaView,
+    Platform,
+    TextInput,
+} from "react-native";
+import axios from "axios";
 
-  const [categories, setCategories] = useState([]);
-  const [location, setLocation] = useState([]);
+const Screen_01 = () => {
+    const [category, setCategory] = useState([]);
+    const [location, setLocation] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [searchFocused, setSearchFocused] = useState(false);
 
-  useEffect(()=>{
-    axios.get("https://6714c6e3690bf212c7626d92.mockapi.io/categories").then((resp)=>{
-      setCategories(resp.data);
-    });
-    axios.get("https://6714c6e3690bf212c7626d92.mockapi.io/location").then((resp)=>{
-      setLocation(resp.data);
-    })
-  },[])
+    const screenWidth = Dimensions.get("window").width;
 
-  return (
-    <View style={styles.container}>
-    {/* begin header */}
-    <View style={styles.header}> 
-      <View style={styles.headerAbow}>
-        <Image style={styles.iconLogo} source={require("../assets/logoicon.png")}/>
-        {/* input */}
-        <View style={styles.input}>
-          <TextInput 
-            placeholder="Search here ..."
-            placeholderTextColor="#bbb"
-          />
-          <Image source={require("../assets/findicon.png")}/>
-        </View>
-      </View>
-      <View style={styles.headerBelow}>
-        <Image style={styles.iconLogo} source={require("../assets/personicon.png")}/>
-        <View>
-          <Text style={{color: "#fff", fontSize: 20, fontWeight: 700}}>Welcome!</Text>
-          <Text style={{color: "#fff", fontSize: 16, fontWeight: 500}}>Donna Stroupe</Text>
-        </View>
-        <Image style={{marginLeft: 100}} source={require("../assets/ringicon.png")}/>
-      </View>
-    </View>
-    {/* end header */}
-    {/* start body */}
-    <View style={styles.body}>
-      <View style={styles.categoryContainer}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems:"center"}}>
-          <Text style={{fontSize: 20, fontWeight: 600}}>Category</Text>
-          <Image style={{width: 30, height: 30}} source={require("../assets/3gach.png")}/>
-        </View>
-        <FlatList 
-        style={styles.categoryItem}
-          data={categories}
-          numColumns={4}
-          keyExtractor={item => item.id.toString()}
-          renderItem={({item}) => (
-            <View>
-              <Image style={{width: 50, height: 50}} source={{uri: item.image}} resizeMode="contain"/>
-              <Text>{item.name}</Text>
+    useEffect(() => {
+        axios
+            .get("https://671ba9d92c842d92c380d3a1.mockapi.io/category")
+            .then((response) => {
+                setCategory(response.data);
+            });
+        axios
+            .get("https://671ba9d92c842d92c380d3a1.mockapi.io/location")
+            .then((response) => {
+                setLocation(response.data);
+            });
+    }, []);
+
+    const numColumns = 4;
+
+    return (
+        <SafeAreaView style={styles.safeArea}>
+            <View style={styles.container}>
+                <ScrollView style={{ width: "100%", height: 500 }}>
+                    {/* Header */}
+                    <View style={styles.headerContainer}>
+                        <View style={styles.header}>
+                            <Image
+                                source={require("../assets/logoicon.png")}
+                                style={styles.logoicon}
+                            />
+                            <View
+                                style={[
+                                    styles.searchBox,
+                                    searchFocused &&
+                                        styles.inputContainerFocused,
+                                ]}
+                            >
+                                <TextInput
+                                    style={styles.searchInput}
+                                    placeholder="Search here..."
+                                    value={searchQuery}
+                                    onFocus={() => setSearchFocused(true)}
+                                    onBlur={() => setSearchFocused(false)}
+                                    onChangeText={setSearchQuery}
+                                />
+                                <Image
+                                    source={require("../assets/findicon.png")}
+                                    style={styles.searchIcon}
+                                />
+                            </View>
+                        </View>
+
+                        <View style={styles.userInfoContainer}>
+                            <View style={styles.userInfo}>
+                                <Image
+                                    source={require("../assets/personicon.png")}
+                                    style={styles.userImage}
+                                />
+                                <View>
+                                    <Text style={styles.welcomeText}>
+                                        Welcome!
+                                    </Text>
+                                    <Text style={styles.userName}>
+                                        Donna Stroupe
+                                    </Text>
+                                </View>
+                            </View>
+                            <Image
+                                source={require("../assets/ringicon.png")}
+                                style={styles.iconBell}
+                            />
+                        </View>
+                    </View>
+
+                    {/* Categories */}
+                    <View style={styles.sectionContainer}>
+                        <Text style={styles.sectionTitle}>Category</Text>
+                        <Image
+                            source={require("../assets/3gach.png")}
+                            style={styles.icon3gach}
+                        />
+                    </View>
+                    <FlatList
+                        data={category}
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity
+                                style={[
+                                    styles.categoryItem,
+                                    { width: screenWidth / numColumns },
+                                ]}
+                            >
+                                <View style={styles.categoryIconContainer}>
+                                    <Image
+                                        source={{ uri: item.image }}
+                                        style={styles.categoryIcon}
+                                    />
+                                </View>
+                                <Text style={styles.categoryText}>
+                                    {item.name}
+                                </Text>
+                            </TouchableOpacity>
+                        )}
+                        numColumns={numColumns}
+                    />
+
+                    {/* Popular Destination */}
+                    <View style={styles.sectionContainer}>
+                        <Text style={styles.sectionTitle}>
+                            Popular Destination
+                        </Text>
+                        <Image
+                            source={require("../assets/3gach.png")}
+                            style={styles.icon3gach}
+                        />
+                    </View>
+                    <FlatList
+                        data={location.slice(0, 3)}
+                        horizontal
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({ item }) => (
+                            <Image
+                                source={{ uri: item.image }}
+                                style={styles.locationImage}
+                            />
+                        )}
+                    />
+
+                    {/* Recommended */}
+                    <View style={styles.sectionContainer}>
+                        <Text style={styles.sectionTitle}>Recommended</Text>
+                    </View>
+                    <FlatList
+                        data={location.slice(3, 5)}
+                        horizontal
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({ item }) => (
+                            <Image
+                                source={{ uri: item.image }}
+                                style={styles.locationImageOfRec}
+                            />
+                        )}
+                    />
+                </ScrollView>
+
+                {/* Footer or bottom nav */}
+                <View style={styles.bottomNav}>
+                    <TouchableOpacity style={styles.navItem}>
+                        <Image
+                            source={require("../assets/homeicon.png")}
+                            style={styles.navicon}
+                        />
+                        <Text style={styles.navLabel}>Home</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.navItem}>
+                        <Image
+                            source={require("../assets/exploreicon.png")}
+                            style={styles.navicon}
+                        />
+                        <Text style={styles.navLabel}>Explore</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.navItem}>
+                        <Image
+                            source={require("../assets/searchicon.png")}
+                            style={styles.navicon}
+                        />
+                        <Text style={styles.navLabel}>Search</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.navItem}>
+                        <Image
+                            source={require("../assets/profileicon.png")}
+                            style={styles.navicon}
+                        />
+                        <Text style={styles.navLabel}>Profile</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
-          )}
-         />
-      </View>
-      <View style={styles.popularContainer}>
-        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems:"center"}}>
-          <Text style={{fontSize: 20, fontWeight: 600, alignItems: 'center'}}>Popular Destination</Text>
-          <Image style={{width: 30, height: 30}} source={require("../assets/3gach.png")}/>
-          </View>
-          <View>
-          <FlatList
-            data={location.slice(0,3)}
-            keyExtractor={item=> item.id}
-            renderItem={({item})=>(
-              <Image style={{width: 50, height: 50}} source={{uri: item.image}} resizeMode="contain"/>
-            )}
-            numColumns={3}
-          />
-        </View>
-      </View>
-      <View>
-        <Text>Recommended</Text>
-        <FlatList
-          data={location.slice(3,5)}
-          keyExtractor={i=> i.id}
-          numColumns={2}
-          renderItem={({item})=>(
-              <Image style={{width: 50, height: 50}} source={{uri: item.image}} resizeMode="contain"/>
-            )}
-        />
-      </View>
-    </View>
-    {/* end body */}
-    <Footer/>
-    </View>
-  );
-}
-
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-  },
-  header:{
-    backgroundColor: "#5958b2",
-    paddingVertical: 20,
-    paddingHorizontal: 20,
-    justifyContent: "center",
-    alignItems: 'center',
-  },
-  headerAbow:{
-    flexDirection:"row",
-    marginBottom: 20
-  },
-  iconLogo:{
-    borderRadius: 50,
-    width: 50,
-    height: 50,
-    marginRight: 15,
-  },
-  input:{
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    borderRadius: 10,
-    padding: 10,
-    backgroundColor: "#fff",
-    flex: 1
-  },
-  headerBelow:{
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center'
-  },
-  body:{
-    paddingHorizontal: 20,
-  },
-  categoryContainer:{
-    marginTop: 20
-  },
-  popularContainer:{
-    marginTop: 20
-  },
-  categoryItem:{
-    padding: 10
-  }
-})
+    safeArea: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        paddingTop: Platform.OS === "android" ? 25 : 0,
+    },
+    headerContainer: {
+        backgroundColor: "#5958b2",
+        height: 220,
+    },
+    header: {
+        padding: 20,
+        marginTop: 24,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    logoicon: {
+        width: 50,
+        height: 50,
+    },
+    searchBox: {
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: "#fff",
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 10,
+        padding: 15,
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    inputContainerFocused: {
+        borderColor: "#1f1f1f",
+        borderWidth: 1,
+    },
+    searchInput: {
+        backgroundColor: "transparent",
+        outlineWidth: 0,
+        flex: 1,
+    },
+    searchIcon: {
+        width: 20,
+        height: 20,
+    },
+    userInfoContainer: {
+        paddingRight: 30,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    userInfo: {
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 20,
+        paddingLeft: 23,
+    },
+    userImage: {
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    welcomeText: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "#fff",
+        marginLeft: 10,
+    },
+    userName: {
+        fontSize: 16,
+        color: "#ddd",
+        marginLeft: 10,
+    },
+    iconBell: {
+        width: 50,
+        height: 50,
+    },
+    sectionContainer: {
+        padding: 20,
+        paddingLeft: 10,
+        paddingRight: 30,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    sectionTitle: {
+        fontSize: 20,
+        margin: 10,
+        textAlign: "left",
+    },
+    icon3gach: {
+        width: 30,
+        height: 30,
+    },
+    categoryItem: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 10,
+    },
+    categoryIconContainer: {
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: "#6C63FF",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    categoryIcon: {
+        width: 64,
+        height: 64,
+    },
+    categoryText: {
+        marginTop: 8,
+        fontSize: 14,
+        color: "#333",
+        textAlign: "center",
+    },
+    locationImage: {
+        width: 122,
+        height: 122,
+        margin: 10,
+        borderRadius: 10,
+    },
+    locationImageOfRec: {
+        width: 192,
+        height: 192,
+        borderRadius: 10,
+        margin: 10,
+    },
+    bottomNav: {
+        flexDirection: "row",
+        justifyContent: "space-around",
+        backgroundColor: "#5958b2",
+        padding: 25,
+    },
+    navItem: {
+        alignItems: "center",
+    },
+    navLabel: {
+        color: "#fff",
+        fontSize: 14,
+        marginTop: 4,
+    },
+    navicon: {
+        width: 40,
+        height: 40,
+    },
+});
+
+export default Screen_01;
